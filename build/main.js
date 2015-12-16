b27c9584085fba6bf39ea95321807f4c7971eae6
Suhair Zain <suhairzain11@gmail.com>
2015-12-16 15:50:54 +0530

    initial Working NPM and Babel commit

---
+++ build/main.js
@@ -0,0 +1,5 @@
+ReactDOM.render(React.createElement(
+  'h1',
+  null,
+  'React Works! Along with Babel'
+), document.getElementById('example'));
---
+++ src/main.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+  <h1>React Works! Along with Babel</h1>,
+  document.getElementById('example')
+);
